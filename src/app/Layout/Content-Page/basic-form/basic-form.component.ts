81cd755e54ff5b0626761647c31724ebcf7f50f4
Hardik Chauhan <hardik@dotin.us>
2019-07-16 18:24:33 +0530

    stepper-changes

--- src/app/Layout/Content-Page/basic-form/basic-form.component.ts
+++ src/app/Layout/Content-Page/basic-form/basic-form.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+declare var jQuery: any;
 
 @Component({
   selector: 'app-basic-form',
   templateUrl: './basic-form.component.html',
   styleUrls: ['./basic-form.component.scss']
 })
-export class BasicFormComponent implements OnInit {
+  
+export class BasicFormComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+    
+    jQuery('.i-checks').iCheck({      
+      checkboxClass: 'icheckbox_square-green',
+      radioClass: 'iradio_square-green',
+    });
+  }
 }
